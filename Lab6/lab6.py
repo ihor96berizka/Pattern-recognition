01ae0b3dd0c9cd45bc46bd238771b6d4e0aeaa59
Ihor Berizka <ihor.berizka@gmail.com>
2018-04-19 00:07:55 +0300

    Added calculation of intersection of clusters with gauss distribution

--- Lab6/lab6.py
+++ Lab6/lab6.py
@@ -10,17 +10,15 @@ M = 3
 mu = []
 sigma = []
 #кількість образів в одному кластері
-n_points = 100
+n_points = 10000
 #к-кісь образів в кожному класі
 N = []
 
-#вектори з ознаками. поки що 2 - x ta y
+#3D матриця з всіма даними. 
 data = []
 
-
-#x1 = []
-#x2 = []
-#x3 = []
+#вектор із обчисленими крайніми точками класу по заданій ознаці
+vidr = []
 
 for i in range(M):
     mu.append([])
@@ -30,18 +28,42 @@ for i in range(M):
         sigma[i].append(np.random.uniform(0, 1))
     N.append(n_points)
     data.append(list())
+    vidr.append(list())
         
 #пройти по всіх кластерах
 for i in range(M):
 #пройти по всіх ознаках
     for j in range(k):
+        #згенерувати вектор розміром N[i] випадкових чисел із заданим mu i sigma
         data[i].append(np.random.normal(mu[i][j], sigma[i][j], N[i]))
 
     if k == 2:
         plt.scatter(data[i][0], data[i][1])
     print("Cluster # ", i)
     for j in range(k):
-        print("M[x", j, "] = ", np.mean(data[i][j]))
-        print("D[x", j, "] = ", np.std(data[i][j])) 
+        curr_mean = np.mean(data[i][j])
+        curr_std = np.std(data[i][j])
+        print("M[x", j, "] = ", curr_mean)
+        print("D[x", j, "] = ", curr_std)
+        x1 = curr_mean - 3 * curr_std
+        x2 = curr_mean + 3 * curr_std
+        vidr[i].append([x1, x2])
+        print("vidr[",i, "|", j, "] =", vidr[i][j])
 
+#пройти по всіх кластерах. попарно порахувати чи перетинаються класи по заданій ознаці
+for i in range(M):
+    for j in range(i+1, M):   
+    #по всіх ознаках
+        for g in range(k):
+            print("Перевірка: ")
+            print("i = ", i, "j = ", j, "g = ", g)
+            print(vidr[i][g][1]," >=", vidr[j][g][0], "and ", vidr[j][g][1], ">= ",vidr[i][g][0])
+            
+            if vidr[i][g][1] >= vidr[j][g][0] and vidr[j][g][1] >= vidr[i][g][0]:
+                print("Cluster # ", i, "and Cluster # ", j,
+                      "Перетинаються по ознаці № ", g)
+            else:
+                print("Cluster # ", i, "and Cluster # ", j,
+                      "НЕ Перетинаються по ознаці № ", g)
+        
 plt.show()
